224d8e1812d5707fb50613de96f5222b255e2773
shripal <shripalp@gmail.com>
2022-11-04 15:56:24 -0600

    bloglist frontend 5.3 step

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -5,7 +5,9 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newBlog, setNewBlog] = useState("a new blog...");
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] =useState('')
   const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
@@ -52,6 +54,32 @@ const App = () => {
     </form>      
   )
 
+
+  const blogForm = () => (
+    <form onSubmit={createBlog}>
+      <label for="title">title</label>
+      <input
+        id="title"
+        value={title}
+        onChange={handleTitleChange}
+      /><br></br>
+      <label for="author">author</label>
+       <input
+       id="author"
+        value={author}
+        onChange={handleAuthorChange}
+      /><br></br>
+      <label for="url">url</label>
+       <input
+       id="url"
+        value={url}
+        onChange={handleUrlChange}
+      /><br></br>
+      
+      <button type="submit">create</button>
+    </form>  
+  )
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -77,8 +105,50 @@ const App = () => {
   const handleLogout =  () => {
     window.localStorage.removeItem('loggedBlogappUser')
     setUser(window.localStorage.getItem('loggedBlogappUser'))
-
   }
+
+  const createBlog = (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url
+    };
+
+    blogService
+      .create(blogObject)
+      .then((returnedBlog) => {
+        setBlogs(blogs.concat(returnedBlog));
+        
+      })
+      .catch((error) => {
+        setErrorMessage(`${error.message}`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        
+      });
+
+    console.log("button clicked", event.target);
+  };
+
+  const handleTitleChange = (event) => {
+    console.log(event.target.value);
+    //console.log("new title is...", newTitle);
+    setTitle(event.target.value);
+  };
+
+  const handleAuthorChange = (event) => {
+    console.log(event.target.value);
+    //console.log("new note is...", newNote);
+    setAuthor(event.target.value);
+  };
+
+  const handleUrlChange = (event) => {
+    console.log(event.target.value);
+    //console.log("new note is...", newNote);
+    setUrl(event.target.value);
+  };
 
   return (
     <div>
@@ -86,19 +156,21 @@ const App = () => {
       {user === null ?
       loginForm() :
       
-      <>
+      <div>
        <p>
         {user.name} logged-in
         <button onClick={handleLogout}>logout</button>
        </p>
-      </>
+       <h2>create new</h2>
+        {blogForm()}
+        {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+      </div>
                  
        }
       
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+      </div>
   )
 }
 
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -7,11 +7,20 @@ const setToken = newToken => {
   token = `bearer ${newToken}`
 }
 
+const create =  async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+};
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 const blogService = {
+  create:create,
   setToken:setToken,
   getAll:getAll
 }
